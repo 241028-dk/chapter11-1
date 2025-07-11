2220a5d5e74ea805fac29ed608be52078da1f8ab
241028 <241028@std.hi-joho.ac.jp>
2025-07-11 12:22:57 +0900

    課題

--- src/Creature.java
+++ src/Creature.java
@@ -1,3 +1,3 @@
-public class Creature {
+public interface Creature {
         void run();
 }
--- src/Fool.java
+++ src/Fool.java
@@ -1,20 +1,18 @@
-public class Fool extends Character implements Human {
-
+public class Fool extends Character implements Human{
     public Fool(String name,int hp) {
-            this.name = "怠け者";
-            this.hp = 80;
+        this.name = "怠け者";
+        this.hp = 80;
     }
-
-    public void talk(Matango m) {
+    public void talk() {
         System.out.println(this.name + "は意味のないことえを話した");
     }
     public void watch() {
         System.out.println(this.name + "は虚空を見つめている");
     }
-    public void here() {
+    public void hear() {
         System.out.println(this.name + "は誰かの声を聞いた気がした");
     }
-    public void attack() {
+    public void attack(Matango m) {
         System.out.println(this.name + "は戦わず遊んでいる");
     }
     public void run() {
